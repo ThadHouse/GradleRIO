8debc9c943279043024ddf99845e5d6a92f40d23
Gold856 <117957790+Gold856@users.noreply.github.com>
2026-08-27 08:51:06 -0400

    Remove Java tool code (#848)

--- src/main/java/org/wpilib/gradlerio/wpi/dependencies/tools/WPIToolsPlugin.java
+++ src/main/java/org/wpilib/gradlerio/wpi/dependencies/tools/WPIToolsPlugin.java
@@ -13,11 +13,9 @@ public class WPIToolsPlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
-        project.getConfigurations().maybeCreate("wpiTools");
         project.getConfigurations().maybeCreate("wpiCppTools");
 
         WPIExtension wpi = project.getExtensions().getByType(WPIExtension.class);
-        List<WPITool> tools = new ArrayList<>();
         List<WPICppTool> cppTools = new ArrayList<>();
 
         Provider<Directory> wpilibHome = wpi.getWpilibHome();
@@ -37,16 +35,9 @@ public class WPIToolsPlugin implements Plugin<Project> {
         cppTools.add(new WPICppTool(project, "wpical", wpi.getVersions().getWpicalToolVersion(),
                 "org.wpilib.tools:wpical", toolsFolder));
 
-        cppTools.add(new WPICppTool(project, "processstarter", wpi.getVersions().getProcessstarterToolVersion(),
-                "org.wpilib.tools:processstarter", toolsFolder));
-
         project.getTasks().register("InstallAllTools", task -> {
             task.setGroup("GradleRIO");
             task.setDescription("Install All Tools");
-
-            for (WPITool tool : tools) {
-                task.dependsOn(tool.getToolInstallTask());
-            }
         });
     }
 }
